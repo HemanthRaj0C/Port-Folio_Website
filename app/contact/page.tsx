e9fe22511785c86f0dfbfbed8bdff112b05f32c0
HemanthRaj0C <145132764+HemanthRaj0C@users.noreply.github.com>
2024-02-04 01:21:47 +0530

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
+		href: "https://twitter.com/Star_King_YT",
+		label: "X",
+		handle: "@Star_King_YT",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:personalhemanthraj@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "dev@personalhemanthraj@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/HemanthRaj0C",
 		label: "Github",
-		handle: "chronark",
+		handle: "HemanthRaj0C",
 	},
 ];
 
